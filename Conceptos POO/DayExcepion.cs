9e053094fad5ea22a7126d9c7885f9a4f50080dc
Usuario <Usuario@DESKTOP-5F383T9>
2023-03-23 08:03:18 -0600

    Agregar archivos de proyecto.

---
+++ Conceptos POO/DayExcepion.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConceptosPOO
+{
+    [Serializable]
+    internal class DayExcepion : Exception
+    {
+        public DayExcepion()
+        {
+        }
+
+        public DayExcepion(string? message) : base(message)
+        {
+        }
+
+        public DayExcepion(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DayExcepion(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
